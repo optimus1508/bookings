af3128358cf62cec395c690706239d0f1975329e
sachimi1508 <59707201+optimus1508@users.noreply.github.com>
2023-05-10 19:24:43 +0200

    Big push after updating our tests + beginning of mail sending protocole

---
+++ internal/repository/dbRepo/test-repo.go
@@ -0,0 +1,43 @@
+package dbrepo
+
+import (
+	"errors"
+	"time"
+
+	"github.com/optimus1508/bookings/internal/models"
+)
+
+func (m *testDBRepo) AllUsers() bool {
+	return true
+}
+
+// insertreservation inserts reservation into the database
+func (m *testDBRepo) InsertReservation(res models.Reservation) (int, error) {
+	return 1, nil
+}
+
+// InsertRoomRestriction inserts a room restriction into the database
+func (m *testDBRepo) InsertRoomRestriction(r models.RoomRestriction) error {
+	return nil
+}
+
+// SearchAvailabilityByDatesByRoomID returns true if there is availability for roomID and false when there isn't
+func (m *testDBRepo) SearchAvailabilityByDatesByRoomID(start, end time.Time, roomID int) (bool, error) {
+	return false, nil
+}
+
+// SearchAvailabilityForAllRooms returns a slice of available rooms if any, for given date range
+func (m *testDBRepo) SearchAvailabilityForAllRooms(start, end time.Time) ([]models.Room, error) {
+	var rooms []models.Room
+	return rooms, nil
+}
+
+// GetRoomByID gets a room by its id
+func (m *testDBRepo) GetRoomByID(id int) (models.Room, error) {
+	var room models.Room
+	if id > 2 {
+		return room, errors.New("Some error")
+	}
+
+	return room, nil
+}
